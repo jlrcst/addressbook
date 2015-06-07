73b2e868a93614d41c013c9ea4d406ee8fb2041e
jose <jose@JoseUltra24>
2015-06-07 13:59:41 +0100

    - Add simple implementation for AddressBook - Remove mock licence for AddressBook and Contact

---
+++ src/main/java/com/gt/addressbook/model/SimpleAddressBook.java
@@ -0,0 +1,41 @@
+package com.gt.addressbook.model;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author jose
+ */
+public class SimpleAddressBook implements AddressBook {
+
+    @Override
+    public int getFemales() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getMales() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Contact getOlder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public HashMap<String, Contact> getContacts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addContact(Contact contact) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void removeContact(String contactName) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
